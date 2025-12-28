c65ba8e5c8dcddafadbd869b4a6f3941991f4734
Romana Peroš <romanaperos@gmail.com>
2025-12-28 07:27:21 +0100

    Committing application development progress

--- decision_paralysis_tools/to_do_to_done.py
+++ decision_paralysis_tools/to_do_to_done.py
@@ -15,58 +15,66 @@ def create_to_do_list():
             adding_tasks = False
 
     to_do_list.remove("q")
-        
+
     print()
-    print("++++++++++++++++++++++++++++++++++++++++")
-    print("Your to do list was succesfully created!")
-    print("++++++++++++++++++++++++++++++++++++++++")
+    print(" +++ Your to do list was succesfully created! +++ ")
     print()
 
     for task in to_do_list:
-        print(f"[] {task}")
+        print(f"[ ] {task}")
 
     print()
-    print("++++++++++++++++++++++++++++++++++++++++")
-    print("        You got this! Go get'em!        ")
-    print("++++++++++++++++++++++++++++++++++++++++")
+    print(" +++ You got this! Go get'em! +++ ")
     print()
-
-create_to_do_list()
 
 # task selection
 
-chosen_task = 0 # OMYGOD THIS IS WHY IT WAS SHOWING ZERO!
+chosen_task = 0
+done_list = []
 
 def task_selection():
     chosen_task = random.choice(to_do_list)
 
     print(f"Your next task is: {chosen_task}")
     print()
-    print("++++++++++++++++++++++++++++++++++++++++")
-    print("            You can do this!            ")
-    print("++++++++++++++++++++++++++++++++++++++++")
+    print(" +++ You can do this! +++ ")
     print()
 
-task_selection()
-
-# migration of tasks to the done list
-
-# this would become function 3
-done_list = []
-
-def create_done_list(chosen_task):
-    
     done_list.append(chosen_task)
+    to_do_list.remove(chosen_task)
+
+# this feels like cheating xD
+
+def create_done_list():
     
     for chosen_task in done_list:
         print(f"[✓] {chosen_task}")
 
-create_done_list(chosen_task)
+def main():
+    create_to_do_list()
+    task_selection()
+    create_done_list()
+    
+    unfinished_tasks = input("Would you like another task? (y/n): ").lower()
 
-# UAAAAARGH HERE WE GO WITH THE VARIABLE SCOPE AGAIN LMAO T_T
-# OKAY WE NEED FUNCTIONS? Which means I'll have to tackle this variable scope issue after all
-# VISION:
-# function 1, 2, 3, call order: 1, 2, 3, 2, 3, 2, 3, 2, 3 --> I wanna do this and repeat it as many times as there are tasks
-# THERE WE GO WITH THE SQUIGGLY LINE BECAUSE OF VARIABLE SCOPE YAY 
-# come on Mana, you can do this, you need to learn this sooner or later
-# how do I "export" the chosen_task variable back to global after it was assigned a random value? 
+    while unfinished_tasks == "y":
+        task_selection()
+        create_done_list()
+
+        print()
+        print(" +++ Well done! I'm so proud of you! +++ ")
+        print()
+
+        unfinished_tasks = input("Would you like another task? (y/n): ").lower()
+
+        if unfinished_tasks == "n" or to_do_list == []:
+            break
+        else:
+            print("Please enter y or n!")
+
+    print()
+    print(" +++ Well done! I'm so proud of you! +++ ")
+    print()
+
+if __name__ == '__main__':
+    main()
